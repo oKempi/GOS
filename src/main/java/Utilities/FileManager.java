168d01ff77f985062ed5ebea80e66148ba615787
Jakub Kvapil <toustovac.cz@gmail.com>
2025-07-10 22:38:50 +0200

    Added

--- src/main/java/Utilities/FileManager.java
+++ src/main/java/Utilities/FileManager.java
@@ -3,6 +3,6 @@ package Utilities;
 import java.io.File;
 import java.io.IOException;
 
-public class FileManager {
+public class FileManager {//will be used to save and load data from .json files
 
 }
