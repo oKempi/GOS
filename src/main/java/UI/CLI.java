d579d2d4797485fdae3bb7a904db1f8287834a1a
Jakub Kvapil <toustovac.cz@gmail.com>
2025-07-10 21:58:41 +0200

    Initial commit

---
+++ src/main/java/UI/CLI.java
@@ -0,0 +1,54 @@
+package Core;
+
+import java.util.Scanner;
+
+public class CLI {
+    private static Boolean monthChanged = false;
+    private static Boolean seasonChanged = false;
+    private static Boolean yearChanged = false;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void print(){
+        //Day/Date <= 1st line
+        if(World.getMonth() == 0){
+            System.out.println("Day " + World.getDay());
+        }
+        if(monthChanged)
+        {System.out.println("It is now " + World.getMonth());}
+
+        if(monthChanged && seasonChanged)
+        {System.out.println(World.getSeason() + " just came around!");}
+
+        if(yearChanged && monthChanged)
+        {System.out.println("Happy new year! It is now year " +  World.getYear());}
+
+        else{System.out.println(World.getDate());}
+
+        //Weather <= 2nd line
+        System.out.println("It is currently" + World.getWeather());
+
+        //Next action in
+        System.out.println("Your next action is in " + Gameloop.nextTurn);
+
+        //Little overview of what has happened
+        resetBooleans();
+    }
+
+    public static void setMonthChanged(){
+        monthChanged = true;
+    }
+    public static void setSeasonChanged(){
+        seasonChanged = true;
+    }
+    public static void setYearChanged(){
+        yearChanged = true;
+    }
+    private static void resetBooleans(){
+        monthChanged = false;
+        seasonChanged = false;
+        yearChanged = false;
+    }
+    public static String getPlayerInput(){
+        return scanner.nextLine();
+    }
+}
