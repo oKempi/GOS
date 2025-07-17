f27041cea47f15f0a8e533547d19283164ab6b0a
Jakub Kvapil <toustovac.cz@gmail.com>
2025-07-17 22:38:44 +0200

    Little change to make it error-less

--- src/main/java/Utilities/FileManager.java
+++ src/main/java/Utilities/FileManager.java
@@ -6,12 +6,11 @@ import java.util.Scanner;
 
 public class FileManager {//will be used to save and load data from files (choices.txt)
 
-    public static String[] read(String filename) {
-        if(filename.equals("situations")) {
-            try{
-                File file = new File("situations.txt");
-
-            }
-        }
-    }
+    //public static String[] read(String filename) {
+    //    if(filename.equals("situations")) {
+    //        try{
+    //            File file = new File("situations.txt");
+    //        }
+    //    }
+    //}
 }
